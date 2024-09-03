20d85259e5a0e9a4195bd4430af4de101741a593
EL Marsho <emarsho@gmail.com>
2024-09-02 22:22:52 -0500

    add styles to .header button + .todo-form input + .todo-list-wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
                         <img src="images/icon-moon.svg" alt="Dark mode toggle icon" className="dark-mode-icon"/>
                     </button>
                 </div>
-                <form>
+                <form className="todo-form">
                     <input
                         type="text"
                         id="new-todo"
@@ -17,35 +17,37 @@ const App = () => {
                         aria-label="Create a new todo"
                     />
                 </form>
-                <ul className="todo-list">
-                    <li>
-                        <input type="checkbox" id="todo-1" />
-                        <label htmlFor="todo-1">Learn React</label>
-                    </li>
-                    <li>
-                        <input type="checkbox" id="todo-2" />
-                        <label htmlFor="todo-2">Build a Todo App</label>
-                    </li>
-                    <li>
-                        <input type="checkbox" id="todo-3" />
-                        <label htmlFor="todo-3">Master JavaScript</label>
-                    </li>
-                    <li>
-                        <input type="checkbox" id="todo-4" />
-                        <label htmlFor="todo-4">Explore Vite</label>
-                    </li>
-                    <li>
-                        <input type="checkbox" id="todo-5" />
-                        <label htmlFor="todo-5">Practice Coding</label>
-                    </li>
-                </ul>
-                <div className="todo-footer">
-                    <p>5 items left</p>
-                    <div className="filter-btn">
-                        <button className="blue-btn" type="button">All</button>
-                        <button type="button">Active</button>
-                        <button type="button">Completed</button>
-                        <button type="button">Clear Completed</button>
+                <div className="todo-list-wrapper">
+                    <ul className="todo-list">
+                        <li>
+                            <input type="checkbox" id="todo-1" />
+                            <label htmlFor="todo-1">Learn React</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="todo-2" />
+                            <label htmlFor="todo-2">Build a Todo App</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="todo-3" />
+                            <label htmlFor="todo-3">Master JavaScript</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="todo-4" />
+                            <label htmlFor="todo-4">Explore Vite</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="todo-5" />
+                            <label htmlFor="todo-5">Practice Coding</label>
+                        </li>
+                    </ul>
+                    <div className="todo-footer">
+                        <p>5 items left</p>
+                        <div className="filter-btn">
+                            <button className="blue-btn" type="button">All</button>
+                            <button type="button">Active</button>
+                            <button type="button">Completed</button>
+                            <button type="button">Clear Completed</button>
+                        </div>
                     </div>
                 </div>
             </div>
